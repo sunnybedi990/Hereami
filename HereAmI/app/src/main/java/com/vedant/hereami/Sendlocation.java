bd0556cb201162b31f133242b8d1d024d338fc64
sunny bedi <sunnybedi@sunnys-MacBook-Air.local>
2017-01-24 23:18:13 +0530

    first commit

---
+++ HereAmI/app/src/main/java/com/vedant/hereami/Sendlocation.java
@@ -0,0 +1,14 @@
+package com.vedant.hereami;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Sendlocation extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sendlocation);
+    }
+
+}
