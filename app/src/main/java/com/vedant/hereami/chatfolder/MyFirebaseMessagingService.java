7d5c7ac00fa6fdadd15b12501374c391c0fce1b3
sunnybedi990 <sunnybedi990@gmail.com>
2017-03-26 02:22:43 +0530

    newone

--- app/src/main/java/com/vedant/hereami/chatfolder/MyFirebaseMessagingService.java
+++ app/src/main/java/com/vedant/hereami/chatfolder/MyFirebaseMessagingService.java
@@ -15,28 +15,69 @@
  */
 package com.vedant.hereami.chatfolder;
 
+import android.content.Intent;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.vedant.hereami.firebasepushnotification.MainActivity5;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
-    private static final String TAG = "MyFMService";
+
+    private static final String TAG = "MyFirebaseMsgService";
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // Handle data payload of FCM messages.
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-
-        // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-        }
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            Log.e(TAG, "Data Payload: " + remoteMessage.getData().toString());
+            try {
+                JSONObject json = new JSONObject(remoteMessage.getData().toString());
+                sendPushNotification(json);
+            } catch (Exception e) {
+                Log.e(TAG, "Exception: " + e.getMessage());
+            }
         }
     }
+
+    //this method will display the notification
+    //We are passing the JSONObject that is received from
+    //firebase cloud messaging
+    private void sendPushNotification(JSONObject json) {
+        //optionally we can display the json into log
+        Log.e(TAG, "Notification JSON " + json.toString());
+        try {
+            //getting the json data
+            JSONObject data = json.getJSONObject("data");
+
+            //parsing json data
+            String title = data.getString("title");
+            String message = data.getString("message");
+            String imageUrl = data.getString("image");
+
+            //creating MyNotificationManager object
+            MyNotificationManager mNotificationManager = new MyNotificationManager(getApplicationContext());
+
+            //creating an intent for the notification
+            Intent intent = new Intent(getApplicationContext(), MainActivity5.class);
+
+            //if there is no image
+            if (imageUrl.equals("null")) {
+                //displaying small notification
+                mNotificationManager.showSmallNotification(title, message, intent);
+            } else {
+                //if there is an image
+                //displaying a big notification
+                mNotificationManager.showBigNotification(title, message, imageUrl, intent);
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "Json Exception: " + e.getMessage());
+        } catch (Exception e) {
+            Log.e(TAG, "Exception: " + e.getMessage());
+        }
+    }
+
 }
