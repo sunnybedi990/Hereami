9c1f74539275e75029ad75cc75804e7216fb2711
sunnybedi <sunnybedi990@gmail.com>
2018-03-27 14:35:56 +0530

    Call Direct from phone number

--- app/src/main/java/com/vedant/hereami/chatfolder/viewuuserpropic.java
+++ app/src/main/java/com/vedant/hereami/chatfolder/viewuuserpropic.java
@@ -1,12 +1,17 @@
 package com.vedant.hereami.chatfolder;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -71,6 +76,12 @@ public class viewuuserpropic extends AppCompatActivity {
                 showImage();
             }
         });
+        number.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                call();
+            }
+        });
 
     }
 
@@ -96,6 +107,22 @@ public class viewuuserpropic extends AppCompatActivity {
                 ViewGroup.MarginLayoutParams.MATCH_PARENT));
 
         builder.show();
+    }
+
+    public void call() {
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:" + mobile));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(callIntent);
     }
 
 }
